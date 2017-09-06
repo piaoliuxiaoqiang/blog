b4cf2ccd004ac46b1aed712269777e2e1e8141f9
Your Name <you@example.com>
2017-09-06 16:59:59 +0800

    2017-9-6

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//文章列表页
+Route::get('/posts','PostController@index');
+//文章详情页面 绑定模型posts
+Route::get('/posts/{post}','PostController@show');
+//文章创建页面
+Route::get('/posts/create','PostController@create');
+Route::post('/posts','PostController@store');
+//文章编辑修改页面
+Route::get('/posts/{post}/edit','PostController@edit');
+Route::put('/posts/{post}','PostController@update');
+//文章删除页面
+Route::get('/posts/delete','PostController@delete');
